aa015a5463ed61535b6fbeb8baf7a7207dc312b2
tk3fftk <tktk.stereoman@gmail.com>
2026-08-25 15:38:50 +0900

    fix: remove external slide

--- src/data/manualPosts.ts
+++ src/data/manualPosts.ts
@@ -9,8 +9,6 @@ export const MANUAL_ARTICLE_URLS: Record<string, string> = {
   'https://sre-magazine.net/articles/9/tk3fftk/': '2025-08-01',
   'https://lounge.primenumber.com/entry/n/n9538986c78a1': '2024-12-19',
   'https://findy-tools.io/products/new-relic/4/350': '2024-12-19',
-};
-export const MANUAL_SLIDE_URLS: Record<string, string> = {
   'https://cdcon2020.sched.com/event/dpvl/case-study-how-yahoo-japan-uses-and-contributes-to-screwdriver-at-scale-hiroki-takatsuka-jithin-emmanuel-yahoo-japan':
     '2020-10-07',
 };
@@ -26,10 +24,5 @@ export const MANUAL_POSTS: ManualPostConfig[] = [
     url,
     pubDate,
     contentType: 'article' as const,
-  })),
-  ...Object.entries(MANUAL_SLIDE_URLS).map(([url, pubDate]) => ({
-    url,
-    pubDate,
-    contentType: 'slide' as const,
   })),
 ];
